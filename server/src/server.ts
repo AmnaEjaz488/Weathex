e55f1496a918e1eeb3921a36217b9952b8f0a2ab
amna <amna.ejaz488@gmail.com>
2025-04-11 00:14:10 -0400

    fixed some bugs

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 dotenv.config();
 
-import routes from './routes/index.js';
+import routes from './routes/api/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,3 +14,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+
+console.log('Resolved routes path:', './routes/api/index.js'); 
